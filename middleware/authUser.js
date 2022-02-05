939243e373e536bc8cf2864060597d5bece63090
Lucas Sta Maria <lucas.stamaria@gmail.com>
2022-02-05 00:43:33 -0500

    fix: change middleware to store user
    Enable the middleware function authUser to not only verify that the
    token is correct (and that the user is authenticated), but also to move
    the user into the request for easier access.
    
    Signed-off-by: Lucas Sta Maria <lucas.stamaria@gmail.com>

--- middleware/authUser.js
+++ middleware/authUser.js
@@ -10,11 +10,14 @@ module.exports = function(req, res, next){
     if(!token) return res.status(401).json('No token');
 
     try {
-        jwt.verify(token, JWT_SECRET); 
-        // req.volunteer = verified.volunteer;
-        next()
+        jwt.verify(token, JWT_SECRET, (err, user) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+            req.user = user;
+            next();
+        });
     } catch (err) {
         res.status(400).json('Invalid token')
     }
-    next()
 }
